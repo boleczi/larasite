d63de00cf0649b19cfdd5added579ab6dff68a7f
boleczi <boleczi@gmail.com>
2020-05-17 15:58:38 +0200

    change title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Witaj swiecie
                 </div>
 
                 <div class="links">
